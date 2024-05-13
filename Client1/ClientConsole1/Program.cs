f0b3602096075b01a645f65708e447644dc2cbba
tonydong2005 <tony.googoog@gmail.com>
2024-05-13 11:14:12 -0500

    commit

--- Client1/ClientConsole1/Program.cs
+++ Client1/ClientConsole1/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
- 
-using System.Net.Sockets;  
+﻿using System.Net.Sockets;  
 using System.Net;
 using System.Timers;
 using System.Runtime.InteropServices;
---
+++ README.md
@@ -0,0 +1,2 @@
+# ServerClientTCP
+##Simple test application for running a server, an intermediate client and server, and a client.
